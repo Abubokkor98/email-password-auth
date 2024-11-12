a5a04c2ce18ab6fe4c966a2e0d65c784a4d4122f
Abubokkor98 <mail.abubokkor@gmail.com>
2024-11-12 12:27:35 +0600

    toggle show password and accept terms

--- src/Component/SignUp.jsx
+++ src/Component/SignUp.jsx
@@ -1,34 +1,46 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "../firebase.init";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 export default function SignUp() {
   const [errorMessage, setErrorMessage] = useState("");
   const [success, setSuccess] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleSignUp = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
-    console.log(email, password);
+    const terms = e.target.terms.checked;
+    console.log(email, password, terms);
 
     // reset error message
-    setErrorMessage('');
-     // reset success message
+    setErrorMessage("");
+    // reset success message
     setSuccess(false);
 
-    // password validation
-    if(password.length < 6){
-      setErrorMessage('Password should be 6 characters or longer');
+    // terms validation
+    if(!terms){
+      setErrorMessage('Please accept our terms.');
       return;
+
     }
-    
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
-    if(!passwordRegex.test(password)){
-      setErrorMessage('At least one uppercase, one lowercase, one especial character, one number');
+
+    // password validation
+    if (password.length < 6) {
+      setErrorMessage("Password should be 6 characters or longer");
       return;
     }
 
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+    if (!passwordRegex.test(password)) {
+      setErrorMessage(
+        "At least one uppercase, one lowercase, one especial character, one number"
+      );
+      return;
+    }
 
     // create user
     createUserWithEmailAndPassword(auth, email, password)
@@ -39,7 +51,7 @@ export default function SignUp() {
       .catch((error) => {
         setErrorMessage(error.message);
       });
-      setSuccess(false);
+    setSuccess(false);
   };
   return (
     <div className="w-6/12 mx-auto">
@@ -58,17 +70,23 @@ export default function SignUp() {
               required
             />
           </div>
-          <div className="form-control">
+          <div className="form-control relative">
             <label className="label">
               <span className="label-text">Password</span>
             </label>
             <input
-              type="password"
+              type={showPassword ? "text" : "password"}
               placeholder="password"
               name="password"
               className="input input-bordered"
               required
             />
+            <button
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-3 top-12"
+            >
+              {showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
+            </button>
             <label className="label">
               <a href="#" className="label-text-alt link link-hover">
                 Forgot password?
@@ -76,13 +94,17 @@ export default function SignUp() {
             </label>
           </div>
           <div className="form-control mt-6">
+            <div className="form-control">
+              <label className="label cursor-pointer justify-start">
+                <input type="checkbox" name="terms" className="checkbox checkbox-primary" />
+                <span className="label-text ml-2">Remember me</span>
+              </label>
+            </div>
             <button className="btn btn-primary">Sign-Up</button>
           </div>
         </form>
         {errorMessage && <p className="text-red-700">{errorMessage}</p>}
-        {
-          success && <p className="text-green-700">Sign Up is successful</p>
-        }
+        {success && <p className="text-green-700">Sign Up is successful</p>}
       </div>
     </div>
   );
